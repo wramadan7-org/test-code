1006a18bda95539de1abc9356fcd3998b7937e9f
wramadan7-org <wramadan1203@gmail.com>
2022-04-23 11:22:18 +0700

    utils for compare password

---
+++ src/utils/compare.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcrypt');
+
+const compare = (password, comparePassword) => {
+  const comparing = bcrypt.compareSync(password, comparePassword);
+
+  if (!comparing) return false;
+
+  return true;
+};
+
+module.exports = compare;

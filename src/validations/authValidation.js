2110cd66b2418d182fed06335aa10854d522e2d2
Wahyu Ramadan <wramadan1203@gmail.com>
2022-04-24 17:14:45 +0700

    authValidation

---
+++ src/validations/authValidation.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+
+const register = {
+  body: Joi.object().keys(
+    {
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(3).required(),
+      phoneNumber: Joi.string().min(12).max(14).pattern(/^\d+$/)
+        .required(),
+    },
+  ),
+};
+
+const login = {
+    body: Joi.object().keys(
+      {
+        email: Joi.string().email().required(),
+        password: Joi.string().min(3).required(),
+      },
+    ),
+  };
+
+module.exports = {
+    register,
+    login,
+};

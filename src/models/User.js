5bc0f0b443c936db77fdb06debfca916c5d27d02
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 10:30:38 +0700

    add phone number in model

--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    phoneNumber: {
+      type: String,
+      required: false,
+    },
     detail: {
       address: {
         type: String,

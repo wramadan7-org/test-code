08949465826e8a225c4b05d2c221497082cc7407
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 13:47:12 +0700

    input validation define in route update own account

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,11 +4,14 @@ const router = Router();
 
 const auth = require('../middlewares/auth');
 const { user } = require('../middlewares/validateRole');
+const validate = require('../middlewares/validate');
+
+const userValidation = require('../validations/userValidation');
 
 const userController = require('../controllers/userController');
 
 router.get('/', auth, user, userController.getOwnAccount);
-router.patch('/', auth, user, userController.updateOwnAccount);
+router.patch('/', auth, user, validate(userValidation.updateOwnAccount), userController.updateOwnAccount);
 router.delete('/', auth, user, userController.deleteOwnAccount);
 
 module.exports = router;

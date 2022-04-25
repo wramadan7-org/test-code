9141a0435d01453ac3059d6b7f2e9a370665d3ef
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 09:27:24 +0700

    use try catch in auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,42 +8,50 @@ const hasing = require('../utils/hashing');
 const userService = require('../services/userService');
 
 const login = async (req, res) => {
-  const loginBody = req.body;
+  try {
+    const loginBody = req.body;
 
-  const user = await userService.getOneUserByQuery({ email: loginBody.email });
+    const user = await userService.getOneUserByQuery({ email: loginBody.email });
 
-  if (!user) return res.sendWrapped('Email not registered.', httpStatus.NOT_FOUND);
+    if (!user) return res.sendWrapped('Email not registered.', httpStatus.NOT_FOUND);
 
-  const checkPassword = await compare(loginBody.password, user.password);
+    const checkPassword = await compare(loginBody.password, user.password);
 
-  if (!checkPassword) return res.sendWrapped('Wrong password', httpStatus.NOT_FOUND);
+    if (!checkPassword) return res.sendWrapped('Wrong password', httpStatus.NOT_FOUND);
 
-  const token = jwt.sign({ id: user.id, email: user.email, role: user.detail.role }, process.env.SECRET_KEY, { expiresIn: '1d' });
+    const token = jwt.sign({ id: user.id, email: user.email, role: user.detail.role }, process.env.SECRET_KEY, { expiresIn: '1d' });
 
-  const data = {
-    token,
-  };
+    const data = {
+      token,
+    };
 
-  res.sendWrapped(data, httpStatus.OK);
+    res.sendWrapped(data, httpStatus.OK);
+  } catch (error) {
+    res.sendWrapped(error.message, httpStatus.BAD_GATEWAY);
+  }
 };
 
 const register = async (req, res) => {
-  const registerBody = req.body;
+  try {
+    const registerBody = req.body;
 
-  const checkEmail = await userService.getOneUserByQuery({ email: registerBody.email });
+    const checkEmail = await userService.getOneUserByQuery({ email: registerBody.email });
 
-  if (checkEmail) return res.sendWrapped('Email already exists.', httpStatus.CONFLICT);
+    if (checkEmail) return res.sendWrapped('Email already exists.', httpStatus.CONFLICT);
 
-  const hash = hasing(registerBody.password, 10);
+    const hash = hasing(registerBody.password, 10);
 
-  const data = {
-    ...registerBody,
-    password: hash,
-  };
+    const data = {
+      ...registerBody,
+      password: hash,
+    };
 
-  const registered = await userService.createUser(data);
+    const registered = await userService.createUser(data);
 
-  res.sendWrapped(registered, httpStatus.CREATED);
+    res.sendWrapped(registered, httpStatus.CREATED);
+  } catch (error) {
+    res.sendWrapped(error.message, httpStatus.BAD_GATEWAY);
+  }
 };
 
 module.exports = {

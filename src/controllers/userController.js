98e8e00b59a194b099ac9e16a5afd14904fcd8ca
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 11:30:12 +0700

    move function create account from controller user to controller admin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,29 +1,6 @@
 const httpStatus = require('http-status');
 
-const bcrypt = require('../utils/hashing');
-
 const userService = require('../services/userService');
-
-const createAccount = async (req, res) => {
-  try {
-    const userBody = req.body;
-
-    const hash = bcrypt(userBody.password, 10);
-
-    const data = {
-      ...userBody,
-      password: hash,
-    };
-
-    const user = await userService.createUser(data);
-
-    if (!user) return res.sendWrapped('Fail to create data', httpStatus.BAD_GATEWAY);
-
-    res.sendWrapped(user, httpStatus.OK);
-  } catch (error) {
-    res.sendWrapped(error.message, httpStatus.BAD_GATEWAY);
-  }
-};
 
 const getOwnAccount = async (req, res) => {
   try {
@@ -130,7 +107,6 @@ const deleteOwnAccount = async (req, res) => {
 };
 
 module.exports = {
-  createAccount,
   getOwnAccount,
   updateOwnAccount,
   deleteOwnAccount,

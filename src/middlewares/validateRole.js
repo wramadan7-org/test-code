3c064e0415063fdf91c575a14fd8572a88f2fa59
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 10:55:32 +0700

    fix conditional in validate role

--- src/middlewares/validateRole.js
+++ src/middlewares/validateRole.js
@@ -2,13 +2,17 @@ const httpStatus = require('http-status');
 
 module.exports = {
   admin: (req, res, next) => {
-    if (!req.user.role === 'admin') return res.sendWrapped('Forbidden access.', httpStatus.FORBIDDEN);
-
-    next();
+    if (req.user.role === 'admin') {
+      next();
+    } else {
+      return res.sendWrapped('Forbidden access.', httpStatus.FORBIDDEN);
+    }
   },
   user: (req, res, next) => {
-    if (!req.user.role === 'user') return res.sendWrapped('Forbidden access', httpStatus.FORBIDDEN);
-
-    next();
+    if (req.user.role === 'user') {
+      next();
+    } else {
+      return res.sendWrapped('Forbidden access', httpStatus.FORBIDDEN);
+    }
   },
 };

e67c69eb4694fb4d55a3b473754d4ee1f7bf3676
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 09:28:23 +0700

    delete ApiError and use res.sendWrapped in validate middleware

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
 const httpStatus = require('http-status');
 const pick = require('../utils/pick');
-const ApiError = require('../utils/ApiError');
 
 const validate = (schema) => (req, res, next) => {
   const validSchema = pick(schema, ['params', 'query', 'body']);
@@ -12,7 +11,7 @@ const validate = (schema) => (req, res, next) => {
 
   if (error) {
     const errorMessage = error.details.map((details) => details.message).join(', ');
-    return next( res.sendWrapped(errorMessage, httpStatus.BAD_REQUEST));
+    return next(res.sendWrapped(errorMessage, httpStatus.BAD_REQUEST));
   }
   Object.assign(req, value);
   return next();

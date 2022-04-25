3e85ef36a276a5b8d95980a2b12c9f96b7c67d1b
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 11:02:45 +0700

    update own profile user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,6 +29,7 @@ const getOwnProfile = async (req, res) => {
   try {
     const { id } = req.user;
     const users = await userService.getOneUserByQuery({ _id: id });
+    console.log(req.user);
 
     res.sendWrapped(users, httpStatus.OK);
   } catch (error) {
@@ -36,7 +37,73 @@ const getOwnProfile = async (req, res) => {
   }
 };
 
+const updateOwnProfile = async (req, res) => {
+  const { id, email, phoneNumber } = req.user;
+  let userBody = req.body;
+
+  let defaultEmail = email;
+  let defaultPhoneNumber = phoneNumber;
+
+  // Check duplicate email
+  if (userBody.email) {
+    const availEmail = await userService.getManyUserByQuery(
+      {
+        $and: [
+          {
+            email: userBody.email,
+          },
+          {
+            email: {
+              $ne: email,
+            },
+          },
+        ],
+      },
+    );
+
+    if (availEmail && availEmail.length > 0) return res.sendWrapped('Email already exists.', httpStatus.CONFLICT);
+
+    defaultEmail = userBody.email;
+  }
+
+  // Check duplicate phone number
+  if (userBody.phoneNumber) {
+    const availPhoneNumber = await userService.getManyUserByQuery(
+      {
+        $and: [
+          {
+            phoneNumber: userBody.phoneNumber,
+          },
+          {
+            phoneNumber: {
+              $ne: phoneNumber,
+            },
+          },
+        ],
+      },
+    );
+
+    if (availPhoneNumber && availPhoneNumber.length > 0) return res.sendWrapped('Phone number already exists.', httpStatus.CONFLICT);
+
+    defaultPhoneNumber = userBody.phoneNumber;
+  }
+
+  const tempData = {
+    phoneNumber: defaultPhoneNumber,
+    email: defaultEmail,
+  };
+
+  Object.assign(userBody, tempData);
+
+  const user = await userService.updateUser(id, userBody);
+
+  if (!user) return res.sendWrapped('Update failed.', httpStatus.BAD_GATEWAY);
+
+  res.sendWrapped(user, httpStatus.OK);
+};
+
 module.exports = {
   createUser,
   getOwnProfile,
+  updateOwnProfile,
 };

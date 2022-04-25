e0305bc10c79c9c11b2e38590677d67132df62e2
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 14:03:08 +0700

    structure code compare

--- src/utils/compare.js
+++ src/utils/compare.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 
 const compare = (password, comparePassword) => {
   const comparing = bcrypt.compareSync(password, comparePassword);
-  
+
   if (!comparing) return false;
 
   return true;

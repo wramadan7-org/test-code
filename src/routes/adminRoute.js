97f2c6aad167b16c3ae17f8647b540792fe295de
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 13:45:39 +0700

    input validation in route when update and create account

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -4,13 +4,16 @@ const router = Router();
 
 const auth = require('../middlewares/auth');
 const { admin } = require('../middlewares/validateRole');
+const validate = require('../middlewares/validate');
+
+const adminValidation = require('../validations/adminValidation');
 
 const adminController = require('../controllers/adminController');
 
-router.post('/', auth, admin, adminController.createAccount);
+router.post('/', auth, admin, validate(adminValidation.createAccount), adminController.createAccount);
 router.get('/', auth, admin, adminController.getAllAccount);
 router.get('/:id', auth, admin, adminController.getAccountById);
-router.patch('/:id', auth, admin, adminController.updateAccountById);
+router.patch('/:id', auth, admin, validate(adminValidation.updateAccount), adminController.updateAccountById);
 router.delete('/:id', auth, admin, adminController.deleteAccountById);
 
 module.exports = router;

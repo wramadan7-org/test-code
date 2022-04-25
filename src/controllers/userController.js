0178f559ace42dbc225a75d6b9f046f5e86d3a82
wramadan7-org <wramadan1203@gmail.com>
2022-04-25 11:27:38 +0700

    put in try catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,86 +39,94 @@ const getOwnAccount = async (req, res) => {
 };
 
 const updateOwnAccount = async (req, res) => {
-  const { id, email, phoneNumber } = req.user;
-  let userBody = req.body;
+  try {
+    const { id, email, phoneNumber } = req.user;
+    let userBody = req.body;
 
-  let defaultEmail = email;
-  let defaultPhoneNumber = phoneNumber;
+    let defaultEmail = email;
+    let defaultPhoneNumber = phoneNumber;
 
-  const checkUser = await userService.getOneUserByQuery({ _id: id });
+    const checkUser = await userService.getOneUserByQuery({ _id: id });
 
-  if (!checkUser) return res.sendWrapped('You don\'t have account', httpStatus.NOT_FOUND);
+    if (!checkUser) return res.sendWrapped('You don\'t have account', httpStatus.NOT_FOUND);
 
-  // Check duplicate email
-  if (userBody.email) {
-    const availEmail = await userService.getManyUserByQuery(
-      {
-        $and: [
-          {
-            email: userBody.email,
-          },
-          {
-            email: {
-              $ne: email,
+    // Check duplicate email
+    if (userBody.email) {
+      const availEmail = await userService.getManyUserByQuery(
+        {
+          $and: [
+            {
+              email: userBody.email,
             },
-          },
-        ],
-      },
-    );
-
-    if (availEmail && availEmail.length > 0) return res.sendWrapped('Email already exists.', httpStatus.CONFLICT);
-
-    defaultEmail = userBody.email;
-  }
-
-  // Check duplicate phone number
-  if (userBody.phoneNumber) {
-    const availPhoneNumber = await userService.getManyUserByQuery(
-      {
-        $and: [
-          {
-            phoneNumber: userBody.phoneNumber,
-          },
-          {
-            phoneNumber: {
-              $ne: phoneNumber,
+            {
+              email: {
+                $ne: email,
+              },
             },
-          },
-        ],
-      },
-    );
+          ],
+        },
+      );
 
-    if (availPhoneNumber && availPhoneNumber.length > 0) return res.sendWrapped('Phone number already exists.', httpStatus.CONFLICT);
+      if (availEmail && availEmail.length > 0) return res.sendWrapped('Email already exists.', httpStatus.CONFLICT);
 
-    defaultPhoneNumber = userBody.phoneNumber;
+      defaultEmail = userBody.email;
+    }
+
+    // Check duplicate phone number
+    if (userBody.phoneNumber) {
+      const availPhoneNumber = await userService.getManyUserByQuery(
+        {
+          $and: [
+            {
+              phoneNumber: userBody.phoneNumber,
+            },
+            {
+              phoneNumber: {
+                $ne: phoneNumber,
+              },
+            },
+          ],
+        },
+      );
+
+      if (availPhoneNumber && availPhoneNumber.length > 0) return res.sendWrapped('Phone number already exists.', httpStatus.CONFLICT);
+
+      defaultPhoneNumber = userBody.phoneNumber;
+    }
+
+    const tempData = {
+      phoneNumber: defaultPhoneNumber,
+      email: defaultEmail,
+    };
+
+    Object.assign(userBody, tempData);
+
+    const user = await userService.updateUser(id, userBody);
+
+    if (!user) return res.sendWrapped('Update failed.', httpStatus.BAD_GATEWAY);
+
+    res.sendWrapped(user, httpStatus.OK);
+  } catch (error) {
+    res.sendWrapped(error.message, httpStatus.BAD_GATEWAY);
   }
-
-  const tempData = {
-    phoneNumber: defaultPhoneNumber,
-    email: defaultEmail,
-  };
-
-  Object.assign(userBody, tempData);
-
-  const user = await userService.updateUser(id, userBody);
-
-  if (!user) return res.sendWrapped('Update failed.', httpStatus.BAD_GATEWAY);
-
-  res.sendWrapped(user, httpStatus.OK);
 };
 
 const deleteOwnAccount = async (req, res) => {
-  const { id } = req.user;
+  try {
+    const { id } = req.user;
 
-  const checkUser = await userService.getOneUserByQuery({ _id: id });
+    const checkUser = await userService.getOneUserByQuery({ _id: id });
 
-  if (!checkUser) return res.sendWrapped('You don\'t have account', httpStatus.NOT_FOUND);
+    if (!checkUser) return res.sendWrapped('You don\'t have account', httpStatus.NOT_FOUND);
 
-  const user = await userService.deleteUserById(id);
+    const user = await userService.deleteUserById(id);
 
-  if (!user) return res.sendWrapped('Fail to delete account', httpStatus.BAD_GATEWAY);
+    if (!user) return res.sendWrapped('Fail to delete account', httpStatus.BAD_GATEWAY);
 
-  res.sendWrapped(user, httpStatus.OK);
+    res.sendWrapped(user, httpStatus.OK);
+  } catch (error) {
+    res.sendWrapped(error.message, httpStatus.BAD_GATEWAY);
+  }
 };
 
 module.exports = {
